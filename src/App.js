51db02eba0b43dd8fac7aafebc6dbede6e76bf11
Artem-Rudko <72283896+Artem-Rudko@users.noreply.github.com>
2021-09-11 00:58:09 +0300

    create folders at components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Component } from 'react';
-import FeedbackOptions from './components/FeedbackOptions';
-import Statistics from './components/Statistics';
-import Section from './components/Section';
+import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
+import Statistics from './components/Statistics/Statistics';
+import Section from './components/Section/Section';
 
 class App extends Component {
     state = {
